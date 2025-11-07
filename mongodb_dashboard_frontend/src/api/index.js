89f0da016e5f2a5d5dd6f123ea5cb3ca02fbcbbf
Kavia AI <production@kavia.ai>
2025-11-07 13:20:10 +0000

    Auto-commit: session cm3bcedb25 - data-management-dashboard-144914-144924

--- mongodb_dashboard_frontend/src/api/index.js
+++ mongodb_dashboard_frontend/src/api/index.js
@@ -2,5 +2,3 @@ export { default as api } from './baseClient';
 export { getApiClient, listUsers, listSessions, listDeployments, listLlmCosts, health } from './baseClient';
 export * from './baseClient';
 export * from './modulesClient';
-export * from './util';
-export { default as httpClient } from './httpClient';
