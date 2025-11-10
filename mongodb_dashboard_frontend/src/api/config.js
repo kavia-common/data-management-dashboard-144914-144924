8942174b918273f9b53843c2d7eaa806b1197bce
Kavia AI <production@kavia.ai>
2025-11-10 16:48:27 +0000

    Checkpoint 67 - cma4c7140a

--- mongodb_dashboard_frontend/src/api/config.js
+++ mongodb_dashboard_frontend/src/api/config.js
@@ -1,7 +1,7 @@
 const apiBase =
   
-  `https://kavia-dashboard-kavia-dev.cloud.kavia.ai/api`;
-  // 'https://vscode-internal-21670-beta.beta01.cloud.kavia.ai:3001/api'
+  // `https://kavia-dashboard-kavia-dev.cloud.kavia.ai/api`;
+  'https://vscode-internal-21670-beta.beta01.cloud.kavia.ai:3001/api'
 
 /**
  * PUBLIC_INTERFACE
