5f8f38a19b0122ceed8441d1f2a88de376298f35
Kavia AI <production@kavia.ai>
2025-12-12 12:39:52 +0000

    Checkpoint 1 - cmec27e582

--- mongodb_dashboard_frontend/src/api/config.js
+++ mongodb_dashboard_frontend/src/api/config.js
@@ -9,7 +9,7 @@
 //
 const apiBase = 
 //  `https://kavia-dashboard-kavia-dev.cloud.kavia.ai/api`;
-'https://vscode-internal-17101-beta.beta01.cloud.kavia.ai:3001/api';
+'https://vscode-internal-29976-beta.beta01.cloud.kavia.ai:3001/api';
 
 /**
  * PUBLIC_INTERFACE
