24c8ffb7165399a9302d82313fbc939ea3a7e07f
Kavia AI <production@kavia.ai>
2025-12-18 06:09:07 +0000

    Checkpoint 10 - cmf9b48c4a

--- mongodb_dashboard_frontend/src/api/config.js
+++ mongodb_dashboard_frontend/src/api/config.js
@@ -9,7 +9,7 @@
 //
 const apiBase = 
 //  `https://kavia-dashboard-kavia-dev.cloud.kavia.ai/api`;
-'https://vscode-internal-36965-beta.beta01.cloud.kavia.ai:3001/api';
+'https://vscode-internal-28054-beta.beta01.cloud.kavia.ai:3001/api';
 
 /**
  * PUBLIC_INTERFACE
