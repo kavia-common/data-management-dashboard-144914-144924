deb85468484ffd796586c472d0f6e4e05510a280
Kavia AI <production@kavia.ai>
2025-12-22 08:11:17 +0000

    Auto-commit: session cmbdb22d9a - data-management-dashboard-144914-144924

--- mongodb_dashboard_frontend/src/api/config.js
+++ mongodb_dashboard_frontend/src/api/config.js
@@ -9,7 +9,7 @@
 //
 const apiBase = 
 //  `https://kavia-dashboard-kavia-dev.cloud.kavia.ai/api`;
-'https://vscode-internal-33088-beta.beta01.cloud.kavia.ai:3001/api';
+'https://vscode-internal-42354-beta.beta01.cloud.kavia.ai:3001/api';
 
 /**
  * PUBLIC_INTERFACE
